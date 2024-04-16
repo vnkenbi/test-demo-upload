99853679dd23c5cda4be639b198310dd54c8f921
vnkenbi <vnkenbi@gmail.com>
2024-04-16 20:16:27 +0700

    video 19

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -8,11 +8,21 @@ class MyComponent  extends React.Component {
         tuoi:43,
         diachi:"Châu Phong"
     }
+    handleClick(event){
+        // console.log("Xin chào")
+        console.log("Bạn đã click vào tôi");
+        console.log("Tên tôi là ", this.state.name)
+    }
+    handleOnmouseOver(event){
+        console.log("Bạn vừa rê chuột vào")
+    }
 
     render(){
         return (
            <div>
             Tôi tên {this.state.name} và đang ở {this.state.diachi}
+            <button onClick={this.handleClick}>Click Me</button>
+            <button onMouseOver={this.handleOnmouseOver}>rê chuột vào tôi</button>
            </div>
         );
     }
