b9053633735a72526e429affc9c5567273a6aa0a
vnkenbi <vnkenbi@gmail.com>
2024-04-16 13:34:42 +0700

    video 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,15 @@
+
+//
+import React from 'react';
+
+class MyComponent  extends React.Component {
+    render(){
+        return (
+            <div>Component dau tien là đây
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
