20cd949e9c616ca8c45a8d3522895f70c6e140b7
vnkenbi <vnkenbi@gmail.com>
2024-04-17 22:52:43 +0700

    video 25

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,33 +3,50 @@
 
 import React from "react";
 
-class DisplayInfo extends React.Component{
-   
+class Display extends React.Component{
+    state={
+        isShowhide: true
+    }
+    handleShowhide=(event)=>{
+        // alert("me")
+        this.setState(
+            {
+                isShowhide:!this.state.isShowhide
+            }
+        )
+        
+    }
     render(){
         const {listUsers}=this.props;
-        // console.log(listUsers);
-        // const {name, tuoi}=this.props
+        
         return(
             <div>
-                
-                {
-                  listUsers.map((duLieuTungDong,index)=>{
-                
-                    return (<div key={duLieuTungDong.id}>
-                            
-                            Tôi tên: {duLieuTungDong.name} <br />
-                            Tuổi tôi là: {duLieuTungDong.tuoi} <br />
-                            <hr/>
-                            </div>) 
+                    <div>
+                        <span onClick={(event)=>{this.handleShowhide(event)}}>{this.state.isShowhide===false ? "Show list user" : "Hide list user" }</span>
+                    </div>
 
-                    
-                    
-                  })  
-                }
-                
+                    { this.state.isShowhide &&
+                    <div>
+                        {
+                            listUsers.map(
+                                (user)=>{
+                                    return (
+                                        <div key={user.id} className={+user.tuoi > 18 ? "green" : "red"}>
+                                                    <div>Tôi tên {user.name}</div>
+                                                    <div>Tuổi: {user.tuoi}</div>
+                                                    <hr/>
+                                        </div>
+                                    );
+                                }
+                            )
+                        }
+                        
+                        
+                    </div>
+                    }
             </div>
         );
     }
 }
-export default DisplayInfo;
+export default Display;
 
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -2,21 +2,24 @@
 
 import React from "react";
 import UserInfo from "./UserInfo";
-import DisplayInfo from "./Display";
+import Display from "./Display";
 
 class Mycomponent extends React.Component{
     state={
-   listUsers: [  {id:1,name:"Nguyễn",tuoi:13},
-        {id:2,name:"Hòa",tuoi:23},
-        {id:3,name:"Lợi",tuoi:33}  ]
+   listUsers: [  
+        {id:1,name:"Nguyễn",tuoi:"13"},
+        {id:2,name:"Hòa",tuoi:"23"},
+        {id:3,name:"Lợi",tuoi:"33"}  
+    ]
     }
     render(){
         return(
             <div>
                 <UserInfo/>
-                <hr/>
-                <DisplayInfo listUsers={this.state.listUsers} />
                 
+                <hr/>
+                <Display listUsers={this.state.listUsers} />
+
                 
             </div>
         );
